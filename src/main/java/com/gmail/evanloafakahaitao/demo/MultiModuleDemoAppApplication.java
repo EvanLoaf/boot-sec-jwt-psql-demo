8162bb14a218e9fb33d2488b3a43cbcc5be3e007
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-06-12 10:48:08 +0300

    first commit

---
+++ src/main/java/com/gmail/evanloafakahaitao/demo/MultiModuleDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.evanloafakahaitao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModuleDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/evanloafakahaitao/demo/MultiModuleDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.evanloafakahaitao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
